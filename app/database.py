e24e6a3bf61e2f78ee7707dedf18da22be2dc379
Micheal-Onyinye <michealphilominaprecious@gmail.com>
2026-05-05 15:08:40 +0100

    Refactor

--- app/database.py
+++ app/database.py
@@ -10,6 +10,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from .database import engine, Base
 from app.routes import expenses, reports, auth
 from fastapi.middleware.cors import CORSMiddleware
+from app.database import DATABASE_URL
 
 app = FastAPI()
 
@@ -15,7 +16,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
+print("DATABASE_URL:", DATABASE_URL)
 app.include_router(auth.router)
 app.include_router(expenses.router)
 app.include_router(reports.router)
